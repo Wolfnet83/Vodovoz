2385dbdb1e444525333e994de6b6b58209dbe371
Wolfnet83 <andrey.volcov@gmail.com>
2012-07-17 16:34:00 +0300

    Clients controller with some actions

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,13 @@
+class ClientsController < ApplicationController
+  def list
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Vodovoz::Application.routes.draw do
+  get "clients/list"
+
+  get "clients/create"
+
+  get "clients/delete"
+
+  get "clients/update"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ Vodovoz::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'clients#list'
 
   # See how all your routes lay out with "rake routes"
 
