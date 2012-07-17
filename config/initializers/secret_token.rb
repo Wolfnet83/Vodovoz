f197b192abe990c64303452c8788dd5059bcf0f3
Wolfnet83 <andrey.volcov@gmail.com>
2012-07-17 15:42:46 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vodovoz::Application.config.secret_token = '6cb2ff687e27c6ebb342f9af5ef477dfbf6c6122a1dd55b0daa526fbef45d3e838f1f387a2e43b8da83adf09a3c46f05973143a1b8e80f5d9ba74dbbc3cbbd32'
