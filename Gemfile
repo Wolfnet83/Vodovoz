59ff7793ce9d5d57e9843065170ac391b967e34d
Wolfnet83 <andrey.volcov@gmail.com>
2012-08-15 10:22:40 +0300

    All is working, but smells

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'mysql2'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
+  gem 'rails_best_practices'
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer'
   gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git', :branch => 'static'
--- app/controllers/missed_calls_controller.rb
+++ app/controllers/missed_calls_controller.rb
@@ -5,9 +5,20 @@ class MissedCallsController < ApplicationController
     @date = params[:date].presence || t
     @hour = params[:hour].presence || h
     query = " select src,count(src),A.disP from  asteriskcdrdb.cdr C
-      LEFT OUTER JOIN (SELECT SRC S,COUNT(SRC) CS,DISPOSITION DISP from  asteriskcdrdb.cdr where  dst<>111 and hour(calldate)='$work_hour' and date(calldate)='$work_date' AND DISPOSITION='ANSWERED' and billsec>5 GROUP BY SRC ) A ON A.S=C.SRC where  dst=111 and hour(calldate)='$work_hour' and date(calldate)='$work_date' 
-    group by src
-    HAVING COUNT(A.S)=0;"
+      LEFT OUTER JOIN (SELECT SRC S,COUNT(SRC) CS,DISPOSITION DISP 
+      from  asteriskcdrdb.cdr where  dst<>111 and hour(calldate)=#{@hour} 
+      and date(calldate)=\"#{@date}\" AND DISPOSITION='ANSWERED' and billsec>5 GROUP BY SRC ) A ON A.S=C.SRC 
+      where  dst=111 and hour(calldate)=#{@hour} and date(calldate)=\"#{@date}\" 
+      group by src
+      HAVING COUNT(A.S)=0;"
+    missed_calls = CDR::Call.find_by_sql(query)
+    arr = Array.new
+    missed_calls.each do |missed_call|
+      arr << missed_call.src
+    end
+    arr=arr.to_s[1..-2]
+    query = " select time(calldate) as my_time,src,billsec from asteriskcdrdb.cdr where dst=111 
+    and hour(calldate)=#{@hour} and date(calldate)=\"#{@date}\" and src in (#{arr})";
     @calls = CDR::Call.find_by_sql(query)
     logger.info "*"*80
     logger.info @calls.inspect
