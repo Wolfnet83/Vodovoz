98eec8168d12c906b8f4e1fe903ae6a8d3dcffb6
Andrei Volcov <andrey.volcov@gmail.com>
2013-01-10 09:29:15 +0200

    Fixed reports bug and some improvements

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -18,29 +18,26 @@
     group_by = params[:group_by] || "day"
     if group_by == "day"  
       @date_column = "Час"
-=begin
     query ="select h as 'time',sum(c1)+sum(c2) as 'quantity_all', sum(c1) as quantity_in,sum(c2) as quantity_out, sum(c3) as received, sum(c1)-sum(c3) as missed
     from(
       select hour(A.calldate) as h,1 as c1, 0 as c2,0 as c3
       from `asteriskcdrdb`.`cdr` as A
       where date(A.calldate)=\'#{@date}\'
       and dst =111
-      and billsec > 5 
      union all
       select hour(calldate) as h,0 as c1,1 as c2,0 as c3
       from `asteriskcdrdb`.`cdr`
       where date(calldate)=\'#{@date}' 
-      and src in (select sipname from asterisk.users where sipname<>'')
-      and billsec > 5 
+      and src between 300 and 399
+      and (dcontext = 'vodovoz-department' or dcontext = 'from-internal')
      union all
       select hour(B.calldate) as h,0 as c1,0 as c2,1 as c3
       from `asteriskcdrdb`.`cdr` as B
       where date(B.calldate)=\'#{@date}\'
-      and dst in (select sipname from asterisk.users where sipname<>'')
-      and billsec > 5
-      and dcontext = 'from-internal') as Q
+      and dst between 300 and 399
+      and disposition = 'ANSWERED'
+      and (dcontext = 'vodovoz-department' or dcontext = 'from-internal')) as Q
      group by h"
-=end
     elsif group_by == "week"
       @date_column = "День недели"
     query ="
@@ -51,22 +48,21 @@
       where week(A.calldate,1)=week(\'#{@date}\',1)
       and year(A.calldate)=year(\'#{@date}\')
       and dst =111
-      and billsec > 5 
      union all
       select day(C.calldate) as h,0 as c1,1 as c2,0 as c3
       from `asteriskcdrdb`.`cdr` as C
       where week(C.calldate,1)=week(\'#{@date}\',1)
       and year(C.calldate)=year(\'#{@date}\')
-      and src in (select sipname from asterisk.users where sipname<>'')
-      and billsec > 5 
+      and src between 300 and 399
+      and (dcontext = 'vodovoz-department' or dcontext='from-internal')
      union all
       select day(B.calldate) as h,0 as c1,0 as c2,1 as c3
       from `asteriskcdrdb`.`cdr` as B
       where week(B.calldate,1)=week(\'#{@date}\',1)
       and year(B.calldate)=year(\'#{@date}\')
-      and dst in (select sipname from asterisk.users where sipname<>'')
-      and billsec > 5
-      and dcontext = 'from-internal') as Q
+      and dst between 300 and 399
+      and disposition = 'ANSWERED'
+      and (dcontext = 'vodovoz-department' or dcontext='from-internal')) as Q
      group by h;"
     else 
       @date_column = "Месяц"
@@ -77,20 +73,19 @@
       from `asteriskcdrdb`.`cdr` as A
       where year(A.calldate)=year(\'#{@date}\')
       and dst =111
-      and billsec > 5 
      union all
       select month(C.calldate) as h,0 as c1,1 as c2,0 as c3
       from `asteriskcdrdb`.`cdr` as C
       where year(C.calldate)=year(\'#{@date}\')
-      and src in (select sipname from asterisk.users where sipname<>'')
-      and billsec > 5 
+      and src between 300 and 399
+      and (dcontext = 'vodovoz-department' or dcontext='from-internal')
      union all
       select month(B.calldate) as h,0 as c1,0 as c2,1 as c3
       from `asteriskcdrdb`.`cdr` as B
       where year(B.calldate)=year(\'#{@date}\')
-      and dst in (select sipname from asterisk.users where sipname<>'')
-      and billsec > 5
-      and dcontext = 'from-internal') as Q
+      and dst between 300 and 399
+      and disposition = 'ANSWERED'
+      and (dcontext = 'vodovoz-department' or dcontext='from-internal')) as Q
      group by h;";
     end
      @calls = CDR::Call.find_by_sql(query)
