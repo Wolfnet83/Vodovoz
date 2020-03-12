638bc493c5a9ab71c9dfb6f42e06183e7bf83857
Andrei Volcov <andrey.volcov@gmail.com>
2020-03-12 15:27:25 +0200

    Add unanswered calls from EVA to clients to call table

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '~> 5.0.0'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'mysql2'
+gem 'mysql2', '~> 0.4.10'
 gem 'kaminari'
 gem 'bootstrap-kaminari-views'
 
--- app/models/call.rb
+++ app/models/call.rb
@@ -4,7 +4,7 @@ class Call < ActiveRecord::Base
   self.table_name = "cdr"
   attr_accessor :link, :linkname
   scope :main, -> (date_from, date_to) {where("(calldate >= ? and calldate <= ? + INTERVAL 1 DAY)
-                                                AND (billsec>0 or src between 300 and 399)
+                                                AND (billsec>0 or src between 300 and 399 or src = \'ivoice_eva\')
                                                 AND (dst<>111 or dstchannel='')",date_from,date_to)}
   scope :source, -> (src) {where(src: src) if src.present?}
   scope :dest, -> (dst) {where(dst: dst) if dst.present?}
